280c49336621cc9d14dabf6d350fe28b29d72380
Mica1909 <mikegoitia11@outlook.com>
2024-01-10 14:59:06 +0000

    limpieaza

--- modules/main.tf
+++ modules/main.tf
@@ -1,23 +1,9 @@
-variable "policy_name" {
-  description = "Nombre de la política"
+provider "azurerm" {
+  features = {}
+}
+module "my_resource_group" {
+  source            = "./rg"  # Ruta al directorio de tu módulo
+  resource_group_name = "example-rg"
+  location          = "East US"
 }
 
-variable "policy_display_name" {
-  description = "Nombre para mostrar de la política"
-}
-
-variable "policy_description" {
-  description = "Descripción de la política"
-}
-
-variable "policy_rule" {
-  description = "Regla de la política"
-}
-
-resource "azurerm_policy_definition" "custom_policy" {
-  name        = var.policy_name
-  display_name= var.policy_display_name
-  description = var.policy_description
-
-  policy_rule = var.policy_rule
-}
