7e76c6706fa77910c9a40dc200d9c650c890165c
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-16 15:32:13 -0300

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,11 +1,12 @@
 
+# outputs.tf
 
 output "vnet_id" {
   description = "ID de la red virtual"
-  value       = module.vnet.virtual_network_id
+  value       = module.vnet.id  # Modificado a "id" en lugar de "virtual_network_id"
 }
 
 output "vnet_address_space" {
   description = "Espacio de direcciones de la red virtual"
-  value       = module.vnet.virtual_network_address_space
+  value       = module.vnet.address_space  # Modificado a "address_space"
 }
