d35cb1c99863ebed41eddbe9c6409f8a3f30f7db
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-16 15:55:45 -0300

    Delete modules/rg/variables.tf

--- modules/rg/variables.tf
+++
@@ -1,9 +0,0 @@
-# Archivo variables.tf dentro del directorio "resource_group"
-variable "resource_group_name" {
-  description = "Nombre del grupo de recursos."
-}
-
-variable "location" {
-  description = "Ubicación del grupo de recursos."
-  default     = "East US"
-}
