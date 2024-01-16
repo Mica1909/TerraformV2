00a843b8773838f81b66364da18edc546a839819
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-16 12:22:45 -0300

    sda

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,5 +1,17 @@
-provider "azurerm" {
-  features = {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstatearsa"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
 }
 
 resource "azurerm_resource_group" "example" {
@@ -7,10 +19,10 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.vnet.name
+  location            = azurerm_resource_group.vnet.location
   address_space       = var.address_space
 
   tags = var.tags
