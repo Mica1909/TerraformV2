f5bb81951b2ea6f4c22648350864cb1b2d3dd62d
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-16 15:55:56 -0300

    Update main.tf

--- modules/rg/main.tf
+++ modules/rg/main.tf
@@ -1,10 +1,18 @@
-# Archivo main.tf dentro del directorio "resource_group"
-provider "azurerm" {
-  features = {}
+# main.tf
+
+variable "resource_group_name" {
+  description = "Nombre del grupo de recursos"
 }
 
-resource "azurerm_resource_group" "example" {
+variable "location" {
+  description = "Región de Azure"
+}
+
+resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
 
+output "resource_group_id" {
+  value = azurerm_resource_group.main.id
+}
