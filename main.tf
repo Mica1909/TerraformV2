d0e0a72053f63889ebbeb5bda4830421d6075c7b
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-16 15:38:12 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
-module "vnet" {
-  source              = "./modules/vnet"  # Ruta local al directorio del módulo
-  resource_group_name = "Vnet-Produccion"
-  location            = "East US"
-  vnet_name           = "LandingZone"
-  address_space       = ["10.0.0.0/16"]
-  tags                = {
-    environment = "production"
-  }
+# main.tf
+
+provider "azurerm" {
+  features = {}
 }
+
+module "vnet" {
+  source = "./modules/vnet_module"
+
+  vnet_name          = "my-vnet"
+  address_space      = ["10.0.0.0/16"]
+  location           = "East US"
+  resource_group_name = "my-resource-group"
+}
+
+output "vnet_id" {
+  value = module.vnet.vnet_id
+}
+
