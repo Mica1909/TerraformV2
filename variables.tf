d157dbda584dd6cdbe68b3780f5c80b3eb152442
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-16 15:15:11 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# variables.tf
+
+variable "resource_group_name" {
+  description = "Nombre del grupo de recursos"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Nombre de la red virtual"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Espacio de direcciones de la red virtual"
+  type        = list(string)
+}
