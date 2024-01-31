7a811d63f9aac7cb3fe4634cfd144142ca043377
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-01-31 11:19:26 -0300

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -19,11 +19,13 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
   address_space       = var.address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
 
   tags = var.tags
 }
