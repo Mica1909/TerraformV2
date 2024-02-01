1ee0e3e12de65323e9cf363474e4ea497d9946d4
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-02-01 16:41:27 -0300

    Update main.tf
    Signed-off-by: Mica1909 <145031324+Mica1909@users.noreply.github.com>

--- modules/vne_existentes/main.tf
+++ modules/vne_existentes/main.tf
@@ -1,17 +1,15 @@
-# En el código fuente de tu módulo
-
 resource "azurerm_virtual_network" "existing_vnet" {
   location            = "eastus2"
-  resource_group_name = "arsa-dev-test-vnet"
+  resource_group_name = "int-dev-test"
   name                = var.vnet_name
-  address_space       = ["10.200.128.0/23"]  # Ajusta según la configuración de tu VNet existente
+  address_space       = ["10.200.128.0/23"]
 }
 
 resource "azurerm_subnet" "existing_subnet" {
   name                 = var.subnet_name
-  # resource_group_name  = var.resource_group_name  # Si es necesario para la subred, pero no para la VNet
   virtual_network_name = azurerm_virtual_network.existing_vnet.name
   address_prefixes     = var.subnet_address_prefixes
 }
+
 
 # Otra configuración para la VNet y la subred existentes...
