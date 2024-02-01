dbbe8ffc533f5e93661341bda49bfe872fab552d
Jose-inaxis <158334736+Jose-inaxis@users.noreply.github.com>
2024-02-01 15:11:29 -0300

    Update main.tf
    jose
    
    Signed-off-by: Jose-inaxis <158334736+Jose-inaxis@users.noreply.github.com>

--- modules/vne_existentes/main.tf
+++ modules/vne_existentes/main.tf
@@ -1,20 +1,15 @@
-# modules/vne_existentes/main.tf
+# En el código fuente de tu módulo
 
-variable "vnet_name" {
-  description = "The name of the existing VNET"
-}
-
-variable "subnet_name" {
-  description = "The name of the existing subnet"
-}
-
-resource "azurerm_virtual_network" "existing" {
+resource "azurerm_virtual_network" "existing_vnet" {
   name                = var.vnet_name
-  resource_group_name = var.resource_group_name
+  address_space       = ["10.200.128.0/23"]  # Ajusta según la configuración de tu VNet existente
 }
 
-resource "azurerm_subnet" "existing" {
+resource "azurerm_subnet" "existing_subnet" {
   name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.existing.name
+  resource_group_name  = var.resource_group_name  # Si es necesario para la subred, pero no para la VNet
+  virtual_network_name = azurerm_virtual_network.existing_vnet.name
+  address_prefixes     = var.subnet_address_prefixes
 }
+
+# Otra configuración para la VNet y la subred existentes...
